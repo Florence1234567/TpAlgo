2f8a140709a3fa02db98ae076baf37c2bcc9f6d4
Florence1234567 <119359132+Florence1234567@users.noreply.github.com>
2025-12-12 20:35:35 -0500

    PotionFix

--- src/Objects/Items/Potions/SpeedPotion.h
+++ src/Objects/Items/Potions/SpeedPotion.h
@@ -8,7 +8,7 @@
 
 class SpeedPotion : public PotionItem {
 public:
-    SpeedPotion() : PotionItem("Assets/Images/Objects/Potion4.png", "Speed Potion") {};
+    SpeedPotion() : PotionItem("Assets/Images/Objects/Potion1.png", "Speed Potion") {};
     void DrinkPotion(PlayerCharacter* player) override;
 
 private:
