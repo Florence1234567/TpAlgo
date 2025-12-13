4f535f352944496d21441d13304396a281c9558e
Florence1234567 <119359132+Florence1234567@users.noreply.github.com>
2025-12-12 19:55:20 -0500

    SpeechBox Sprite

--- src/UI/PlayerUI/DialogUI.cpp
+++ src/UI/PlayerUI/DialogUI.cpp
@@ -1,13 +1,23 @@
 #include "DialogUI.h"
 
+void DialogUI::LoadSprite()
+{
+    if (backgroundTexture.loadFromFile(spritePath)) {
+        backgroundSprite = new sf::Sprite(backgroundTexture);
+        backgroundSprite->setScale({ 0.025f, 0.025f });
+    }  
+}
+
 void DialogUI::AddMessage(const std::string msg, sf::Vector2f pos)
 {
     text = msg;
     timer = duration;
-    sf::FloatRect bounds = background.getLocalBounds();
-    background.setOrigin(sf::Vector2f(bounds.size.x / 2.f, bounds.size.y / 2.f));
-    background.setPosition(pos);
-    position = background.getPosition();
+
+    sf::FloatRect bounds = backgroundSprite->getLocalBounds();
+    backgroundSprite->setOrigin(sf::Vector2f(bounds.size.x / 2.f, bounds.size.y / 2.f));
+    backgroundSprite->setPosition({ pos.x + offset.x, pos.y - 30 });
+    position = backgroundSprite->getPosition();
+
     isVisible = true;
 }
 
@@ -27,11 +37,11 @@ void DialogUI::Draw(sf::RenderWindow& window)
     if (!isVisible) 
         return;
 
-    window.draw(background);
+    window.draw(*backgroundSprite);
 
     sf::Text textBox(font, text, 12);
     textBox.setFillColor(sf::Color::Black);
-    sf::Vector2f textPosition = { position.x - background.getLocalBounds().size.x / 2.f + margin, position.y - background.getLocalBounds().size.y / 2.f + margin };
+    sf::Vector2f textPosition = { position.x - background.getLocalBounds().size.x / 2.f + margin - 5 + offset.x, position.y - background.getLocalBounds().size.y / 2.f + margin };
     textBox.setPosition(textPosition);
 
     window.draw(textBox);
